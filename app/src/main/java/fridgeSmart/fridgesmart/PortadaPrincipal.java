43d35a35399fb48816d02a9719d5284637501349
Johana Tonguino <johanatonguino94@gmail.com>
2025-02-07 02:33:58 +0100

    hasta ahora funciona bien he creado la ventana lateral con el logo

--- app/src/main/java/fridgeSmart/fridgesmart/PortadaPrincipal.java
+++ app/src/main/java/fridgeSmart/fridgesmart/PortadaPrincipal.java
@@ -1,17 +1,30 @@
 package fridgeSmart.fridgesmart;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class PortadaPrincipal extends AppCompatActivity {
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+        ImageView btnMenu = findViewById(R.id.btnMenu);
+
+        btnMenu.setOnClickListener(view ->{
+            drawerLayout.open();
+
+        });
 
     }
 }
